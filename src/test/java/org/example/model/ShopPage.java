b139d71dc7ddf742ef63819b7dea2cdb4c02cbea
Ivan Kimlaev <kimlaevan@gmail.com>
2025-04-17 20:01:54 +0700

    del utils

--- src/test/java/org/example/model/ShopPage.java
+++ src/test/java/org/example/model/ShopPage.java
@@ -3,7 +3,6 @@ package org.example.model;
 import com.microsoft.playwright.Locator;
 import com.microsoft.playwright.Page;
 import org.example.utils.CheckBuyUtils;
-import org.example.utils.PurchaseControlUtils;
 
 public class ShopPage {
 
@@ -15,24 +14,30 @@ public class ShopPage {
     public String sortButton = "//select[@class='product_sort_container']";
     public String buttonBuy = "(//div[@id='inventory_container'][1]//button)[%d]";
 
-    public void clickShoppingCartButton() {
-        page.click(shoppingCartButton);
+    public void clickShoppingCartButton() {page.click(shoppingCartButton);}
+
+    public void CheckBuy(int numPurch, String buttonBuy) {
+        String targetText = "Add to cart";
+        int count = 0;
+        for (int i = 1; i < 6; i++) {
+            if (count < numPurch) {
+                String textButton = page.locator(String.format(buttonBuy, i)).textContent();
+                if (targetText.equals(textButton)) {
+                    page.locator(String.format(buttonBuy, i)).click();
+                    count++;
+                }
+            }
+        }
     }
 
     public void clickHiLoButtonSort(int numPurch) {
-        Locator sort = page.locator(sortButton);
-        CheckBuyUtils buy = new CheckBuyUtils(page);
-        sort.click();
-        sort.selectOption("hilo");
-        buy.CheckBuy(numPurch, buttonBuy);
+        page.locator(sortButton).selectOption("hilo");
+        CheckBuy(numPurch, buttonBuy);
     }
 
     public void clickLoHiButtonSort(int numPurch) {
-        Locator sort = page.locator(sortButton);
-        CheckBuyUtils buy = new CheckBuyUtils(page);
-        sort.click();
-        sort.selectOption("lohi");
-        buy.CheckBuy(numPurch, buttonBuy);
+        page.locator(sortButton).selectOption("lohi");
+        CheckBuy(numPurch, buttonBuy);
     }
 
 
--- src/test/java/org/example/utils/CheckBuyUtils.java
+++ src/test/java/org/example/utils/CheckBuyUtils.java
@@ -9,17 +9,5 @@ public class CheckBuyUtils {
         this.page = page;
     }
 
-    public void CheckBuy(int numPurch, String buttonBuy) {
-        String targetText = "Add to cart";
-        int count = 0;
-        for (int i = 1; i < 6; i++) {
-            if (count < numPurch) {
-                String textButton = page.locator(String.format(buttonBuy, i)).textContent();
-                if (targetText.equals(textButton)) {
-                    page.locator(String.format(buttonBuy, i)).click();
-                    count++;
-                }
-            }
-        }
-    }
+
 }
