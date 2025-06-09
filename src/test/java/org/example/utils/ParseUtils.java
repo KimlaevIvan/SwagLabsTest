0e1b692e7cdccab93fcfde8d72909608a3cd04bb
KimlaevIvan <kimlaevan@gmail.com>
2025-06-09 22:56:55 +0700

    debug CardPage

--- src/test/java/org/example/utils/ParseUtils.java
+++ src/test/java/org/example/utils/ParseUtils.java
@@ -1,24 +1,17 @@
 package org.example.utils;
 
+import java.util.Map;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
 public class ParseUtils {
-    public static String pars(String nameField, String text) {
-        String regex = "";
-        String regex_login = "[a-zA-Z0-9]+(_user)";
-        String regex_password = "[a-zA-Z0-9]+(_sauce)";
-        String regex_buy = "[0-9.]+";
-        if (nameField.equals("password")) {
-            regex = regex_password;
-        } else if (nameField.equals("money")) {
-            regex = regex_buy;
-        } else if (nameField.equals("login")) {
-            regex = regex_login;
-        } else {
-            System.out.println("No regex");
-        }
-        Pattern pattern = Pattern.compile(regex);
+    public static String pars (String regex, String text) {
+        Map<String,String> indexMap = Map.of(
+            "password","[a-zA-Z0-9]+(_sauce)",
+            "login","[a-zA-Z0-9]+(_user)",
+                "money","[0-9.]+"
+        );
+        Pattern pattern = Pattern.compile(indexMap.get(regex));
         Matcher matcher = pattern.matcher(text);
         if (matcher.find()) {
             return matcher.group();
